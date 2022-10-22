c45b88b5ef1cf84564b69c75fcfc373084fb3129
Drona <53328757+Drona1@users.noreply.github.com>
2022-10-22 12:22:41 +0300

    homework2

--- src/main/java/org/javastart/homework2/DataTypes.java
+++ src/main/java/org/javastart/homework2/DataTypes.java
@@ -2,8 +2,8 @@ package org.javastart.homework2;
 
 public class DataTypes {
     public static void main(String[] args) {
-
-        System.out.println("\nExamples of Primitive Data Types:");
+        System.out.println();
+        System.out.println("Examples of Primitive Data Types:");
         System.out.println("boolean: " + true);
         System.out.println("int: " + 2_147_483_647);
         System.out.println("long: " + 9_223_372_036_854_775_807L);
@@ -13,14 +13,17 @@ public class DataTypes {
         System.out.print("  " + '\u263A');
         System.out.println("  " + (char) 9786);
 
-        System.out.println("\n12-bit integer data type has a minimum value of " +
+        System.out.println();
+        System.out.println("12-bit integer data type has a minimum value of " +
                 (int) (-Math.pow(2, 12) / 2) + " and a maximum value of " +
                 (int) (Math.pow(2, 12) / 2 - 1) + " (inclusive).");
 
-        System.out.println("\n4-bit character data type can operate with " +
+        System.out.println();
+        System.out.println("4-bit character data type can operate with " +
                 (int) (Math.pow(2, 4)) + " different characters.");
 
-        System.out.println("\nExample of enum:");
+        System.out.println();
+        System.out.println("Example of enum:");
 
         System.out.println(Gender.MALE);
         System.out.println(Gender.FEMALE);
