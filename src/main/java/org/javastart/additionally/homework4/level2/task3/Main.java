dc37d02f1474e6da8b3998d1c69df3d9d4b82635
Drona <53328757+Drona1@users.noreply.github.com>
2022-10-23 22:01:16 +0300

    Additionally homework4

---
+++ src/main/java/org/javastart/additionally/homework4/level2/task3/Main.java
@@ -0,0 +1,57 @@
+/*
+ * 3) Выведите на экран «песочные часы» максимальная ширина которых
+ * считывается с клавиатуры (число нечетное). В примере ширина равна 5.
+ * *****
+ *  ***
+ *   *
+ *  ***
+ * *****
+ */
+package org.javastart.additionally.homework4.level2.task3;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int width = 0;
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("Enter the width of the hourglass" +
+                    " (number must be odd) and press Enter");
+            do {
+                if (scanner.hasNextInt()) {
+                    width = scanner.nextInt();
+                    scanner.nextLine();
+                    if (width % 2 == 0) {
+                        System.out.println("Number must be odd," +
+                                " enter new width");
+                        width = 0;
+                    }
+                } else {
+                    System.out.println("Wrong data, try again");
+                    scanner.nextLine();
+                }
+            } while (width == 0);
+        }
+
+        int increment = 1;
+        int modifier = 0;
+
+        for (int i = 1; i <= width; i++) {
+            for (int j = 1; j <= width; j++) {
+                if (j <= modifier) {
+                    System.out.print(" ");
+                } else if (j <= (modifier + width - modifier * 2)) {
+                    System.out.print("*");
+                }else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+            modifier+=increment;
+            if (modifier == width/2){
+                increment = -1;
+            }
+        }
+    }
+}
